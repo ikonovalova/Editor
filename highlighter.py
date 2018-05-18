d985ab69a90371b956a8942459791fe49f728a60
ikonovalova <irina7l3@mail.ru>
2018-05-18 13:46:38 +0300

    Remove lines

--- highlighter.py
+++ highlighter.py
@@ -4,21 +4,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Highlighter:
     # For text highlighting
     def highlightText(self, start, end, lineColor):
-        # colorFormat =  QtGui.QTextCharFormat()
-        # colorFormat.setBackground(QtGui.QColor(QtCore.Qt.green).lighter(160))
 
         c = self.textCursor()
 
         c.setPosition(start, QtGui.QTextCursor.MoveAnchor)
         c.setPosition(end, QtGui.QTextCursor.KeepAnchor)
 
-        # c.mergeCharFormat(colorFormat)
-        # c.setPosition(end)
-        # self.setTextCursor(c)
-
         extraSelections = []
         selection = QtWidgets.QTextEdit.ExtraSelection()
-        # lineColor = QtGui.QColor(QtCore.Qt.green).lighter(160)
         selection.format.setBackground(lineColor)
         selection.cursor = c
 
